6142eb2f7980f0e98c15daa182bb88a268fd0ccf
Mike Goodman <msgoodman90@gmail.com>
2020-08-10 01:14:57 -0400

    Add autoscaling description textbox and markdown rendering for node description

--- frontend/src/components/PageSections/TopSection/TopSection.js
+++ frontend/src/components/PageSections/TopSection/TopSection.js
@@ -3,23 +3,34 @@ import './TopSection.scss';
 import ContributorSection from './ContributorSection/ContributorSection';
 import DetailSection from './DetailSection/DetailSection';
 import VerticalImageSection from './VerticalImageSection/VerticalImageSection';
+import TextareaAutosize from 'react-textarea-autosize';
+import { updateNode } from '../../../utils/api';
+import ReactMarkdown from "react-markdown";
 
 function TopSection(props) {
+    function updateDescription() {
+        const updatedNode = { ...props.node };
+        updatedNode.markdown_content = newDescription;
+        updateNode(updatedNode).then(updated => props.setNewestNodeUpdate('Updated markdown content'));
+        setTextboxVisible(false);
+    }
+
     const [textboxVisible, setTextboxVisible] = useState(false);
     const [newDescription, setNewDescription] = useState(props.node.markdown_content);
 
     const icon = props.node.icon ? <i className={props.node.icon}></i> : null;
-    const displayDescription = !textboxVisible ? props.node.markdown_content : null;
-    const editDescription = textboxVisible ? <textarea value={newDescription}></textarea> : null;
+    const displayDescription = !textboxVisible ? <ReactMarkdown source={props.node.markdown_content} /> : null;
+    const editDescription = textboxVisible ? <TextareaAutosize value={newDescription} onChange={e => setNewDescription(e.target.value)} /> : null;
 
     const editButton = !textboxVisible ?
         <button onClick={() => setTextboxVisible(true)} className="newTagButton"> <i className="fas fa-pencil-alt"></i> Edit Description </button> : null;
 
     const cancelButton = textboxVisible ?
-        <button onClick={() => setTextboxVisible(false)} className="cancelNewTagButton smallButton"> <i className="fas fa-ban"></i> Cancel Changes </button> : null;
+        <button onClick={() => { setTextboxVisible(false); setNewDescription(props.node.markdown_content); }} className="cancelNewTagButton smallButton" >
+            <i className="fas fa-ban"></i> Cancel Changes </button > : null;
 
     const confirmButton = textboxVisible ?
-        <button onClick={() => console.log('newTagName')} className="confirmNewTagButton smallButton"><i className="fas fa-check"></i> Save Changes</button> : null;
+        <button onClick={updateDescription} className="confirmNewTagButton smallButton"><i className="fas fa-check"></i> Save Changes</button> : null;
 
     return (
         <div className="TopSection">
