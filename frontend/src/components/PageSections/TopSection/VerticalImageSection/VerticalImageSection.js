feb3810edf225bbaa71039b8fbdb93d23958ae21
Mike Goodman <msgoodman90@gmail.com>
2021-09-05 14:52:23 -0400

    More not pulling from local for amplify

--- frontend/src/components/PageSections/TopSection/VerticalImageSection/VerticalImageSection.js
+++ frontend/src/components/PageSections/TopSection/VerticalImageSection/VerticalImageSection.js
@@ -3,13 +3,14 @@ import './VerticalImageSection.scss';
 
 
 function VerticalImageSection(props) {
-    const verticalImage = props.parentNode.vertical_image_uuid ?
-        <div className="verticalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.vertical_image_uuid}.${props.parentNode.vertical_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
-        null;
-
-    const horizontalImage = props.parentNode.horizontal_image_uuid ?
-        <div className="horizontalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.horizontal_image_uuid}.${props.parentNode.horizontal_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
-        null;
+    // const verticalImage = props.parentNode.vertical_image_uuid ?
+    //     <div className="verticalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.vertical_image_uuid}.${props.parentNode.vertical_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
+    //     null;
+    const verticalImage = <div className="verticalImage"> <img src=''/> </div>
+    // const horizontalImage = props.parentNode.horizontal_image_uuid ?
+    //     <div className="horizontalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.horizontal_image_uuid}.${props.parentNode.horizontal_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
+    //     null;
+    const horizontalImage = <div className="horizontalImage"> <img src=''/> </div>
 
     return (
         <div className="VerticalImageSection">
