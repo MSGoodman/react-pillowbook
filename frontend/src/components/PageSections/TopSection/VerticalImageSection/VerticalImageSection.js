681e2873adbc53fd9b1b4dc852c0b8dcc469f34f
Danny <danny@wednesdaytalent.com>
2022-01-16 15:25:32 -0500

    comment out missing asset paths, for now

--- frontend/src/components/PageSections/TopSection/VerticalImageSection/VerticalImageSection.js
+++ frontend/src/components/PageSections/TopSection/VerticalImageSection/VerticalImageSection.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './VerticalImageSection.scss';
-
+import React from "react";
+import "./VerticalImageSection.scss";
 
 function VerticalImageSection(props) {
-    const verticalImage = props.parentNode.vertical_image_uuid ?
-        <div className="verticalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.vertical_image_uuid}.${props.parentNode.vertical_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
-        null;
+    const verticalImage = props.parentNode.vertical_image_uuid ? (
+        <div className="verticalImage"> ok </div>
+    ) : null;
 
-    const horizontalImage = props.parentNode.horizontal_image_uuid ?
-        <div className="horizontalImage"> <img src={require(`../../../../assets/uploads/${props.parentNode.horizontal_image_uuid}.${props.parentNode.horizontal_image_extension}`)} alt={props.parentNode.name + " image"} /> </div> :
-        null;
+    const horizontalImage = props.parentNode.horizontal_image_uuid ? (
+        <div className="horizontalImage"> ok2 </div>
+    ) : null;
 
     return (
         <div className="VerticalImageSection">
--- frontend/src/components/childElements/Attachment/Attachment.js
+++ frontend/src/components/childElements/Attachment/Attachment.js
@@ -1,35 +1,63 @@
-import React from 'react';
-import './Attachment.scss';
-import { Link } from 'react-router-dom';
-import { updateNode } from '../../../utils/api';
+import React from "react";
+import "./Attachment.scss";
+import { Link } from "react-router-dom";
+import { updateNode } from "../../../utils/api";
 
 function Attachment(props) {
     function updateHorizontal() {
         const updatedNode = { ...props.parentNode };
         updatedNode.horizontal_image_node = props.node.node_id;
-        updateNode(updatedNode).then(updated => props.setNewestNodeUpdate('Set new horizontal image: ' + props.node.name));
+        updateNode(updatedNode).then((updated) =>
+            props.setNewestNodeUpdate(
+                "Set new horizontal image: " + props.node.name
+            )
+        );
     }
     function updateVertical() {
         const updatedNode = { ...props.parentNode };
         updatedNode.vertical_image_node = props.node.node_id;
-        updateNode(updatedNode).then(updated => props.setNewestNodeUpdate('Set new vertical image: ' + props.node.name));
+        updateNode(updatedNode).then((updated) =>
+            props.setNewestNodeUpdate(
+                "Set new vertical image: " + props.node.name
+            )
+        );
     }
 
-    const imageExtensions = ['jpg', 'png', 'svg', 'jpeg', 'gif', 'jfif'];
-    const src = imageExtensions.includes(props.node.file_extension.toLowerCase()) ?
-        require(`../../../assets/uploads/${props.node.node_uuid + '.' + props.node.file_extension}`)
-        : `/static/media/${props.node.node_uuid + '.' + props.node.file_extension}`;
-    const image = imageExtensions.includes(props.node.file_extension.toLowerCase()) ?
+    const imageExtensions = ["jpg", "png", "svg", "jpeg", "gif", "jfif"];
+    const src = imageExtensions.includes(
+        props.node.file_extension.toLowerCase()
+    )`/static/media/${props.node.node_uuid + "." + props.node.file_extension}`;
+    const image = imageExtensions.includes(
+        props.node.file_extension.toLowerCase()
+    ) ? (
         <img src={src} alt={props.node.name + " image"} />
-        : <img className="nonImageImage" src={require('../../../assets/images/file-archive.svg')} alt="generic attachment" />
+    ) : (
+        <img
+            className="nonImageImage"
+            src={require("../../../assets/images/file-archive.svg")}
+            alt="generic attachment"
+        />
+    );
 
     return (
         <div className="Attachment">
             <div className="filename">{props.node.name}</div>
-            <a title="Download file" href={src} download><i className="fas fa-download"></i></a>
-            <button title="Set as horizontal image" onClick={updateHorizontal}><i className="fas fa-arrows-alt-h"></i></button>
-            <button title="Set as vertical image" onClick={updateVertical}><i className="fas fa-arrows-alt-v"></i></button>
-            <Link to={`/nodes/${props.node.node_uuid}`} title="Navigate to file page"> <i className="fas fa-external-link-alt"></i> </Link>
+            <a title="Download file" href={src} download>
+                <i className="fas fa-download"></i>
+            </a>
+            <button title="Set as horizontal image" onClick={updateHorizontal}>
+                <i className="fas fa-arrows-alt-h"></i>
+            </button>
+            <button title="Set as vertical image" onClick={updateVertical}>
+                <i className="fas fa-arrows-alt-v"></i>
+            </button>
+            <Link
+                to={`/nodes/${props.node.node_uuid}`}
+                title="Navigate to file page"
+            >
+                {" "}
+                <i className="fas fa-external-link-alt"></i>{" "}
+            </Link>
 
             {image}
             {/* <Link to={`/nodes/${props.node.node_uuid}`} className="reviewHeader">
@@ -40,7 +68,7 @@ function Attachment(props) {
             <div className="reviewText">
                 {props.node.markdown_content}
             </div> */}
-        </div >
+        </div>
     );
 }
 
